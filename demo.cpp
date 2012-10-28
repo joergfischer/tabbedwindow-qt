ee3ab2255fee550ce80f90bb5ce2bd5244c3667b
Daniele Esposti <expo@expobrain.net>
2012-10-28 18:45:55 +0000

    Renamed main file into demo

---
+++ demo.cpp
@@ -0,0 +1,42 @@
+#include "tabbedwindow.h"
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QToolBar>
+#include <QAction>
+#include <QMenuBar>
+
+
+int main(int argc, char *argv[])
+{
+    // Create application
+    QApplication a(argc, argv);
+
+    // Create tabbed views
+    TabbedWindow w;
+    QMainWindow red, green, blue;
+
+    red.setStyleSheet("QMainWindow { background-color: red; }");
+    blue.setStyleSheet("QMainWindow { background-color: blue; }");
+    green.setStyleSheet("QMainWindow { background-color: green; }");
+
+    w.addView(&red, QString("Red View"));
+    w.addView(&blue, QString("Blue view"));
+    w.addView(&green, QString("Green view"));
+
+    // Added test toolbar
+    QToolBar *tb = red.addToolBar("toolbar");
+    tb->addAction("RED");
+
+    // Add test menubar
+    QAction *ma = new QAction("Test", &green);
+    QMenu *m = green.menuBar()->addMenu("File");
+    m->addAction(ma);
+
+    // Set size and show
+    w.resize(400, 400);
+    w.show();
+    
+    // Start mainloop
+    return a.exec();
+}
